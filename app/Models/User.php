fc2b7a42ebac3fea2f5fd0265499d496821b03fd
Ari P <ariteknologi@gmail.com>
2026-01-19 01:26:31 +0700

    fix authorization

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -64,5 +64,10 @@ class User extends Authenticatable
     public function orangTua()
     {
         return $this->hasOne(OrangTua::class, 'user_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
